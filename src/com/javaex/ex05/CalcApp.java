cf8be7afed4c26c227f01ce96abbc2142458f15a
NoWorryYun <dbstjd3201@naver.com>
2022-05-02 00:06:28 +0900

    2022-05-01 practice08

---
+++ src/com/javaex/ex05/CalcApp.java
@@ -0,0 +1,18 @@
+package com.javaex.ex05;
+
+import java.util.Scanner;
+
+public class CalcApp {
+
+    public static void main(String[] args) {
+
+    	Scanner sc = new Scanner(System.in);
+    	
+    		System.out.print(">>");
+    		String num = sc.nextLine();
+    		
+    	
+    	
+    	sc.close();
+    }
+}
